565d16c10a3b8337bc735c3695650558630c593c
pmontgo33 <pmontgo33@gmail.com>
2023-01-05 03:11:27 +0000

    fixed errors in backup.sh. split updates into system and docker.

--- playbooks/templates/backup_script/backups.sh
+++ playbooks/templates/backup_script/backups.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#exec > "/mnt/external/backups/logs/$(date +'%Y-%m-%d')backup.log" 2>&1
+exec > >(tee "/mnt/external/backups/logs/$(date +'%Y-%m-%d') backup.log") 2>&1
 
 echo "================================================="
 echo "Running Scheduled Backup ($(date '+%m/%d/%Y %H:%M:%S'))"
@@ -6,19 +8,21 @@ echo "================================================="
 echo "-"
 
 echo "STARTING rsync server01 backup to latest folder on external..."
-rsync -avhqr --delete --delete-excluded --log-file=/mnt/external/backups/logs/backup.log --files-from=backup_include.dat --exclude-from=backup_exclude.dat / /mnt/external/backups/latest/server01
+rsync -avhqr --delete --delete-excluded --files-from=./backup_include.dat --exclude-from=./backup_exclude.dat / /mnt/external/backups/latest/server01
+
+#--log-file="/mnt/external/backups/logs/$(date +'%Y-%m-%d')server01-backup.log"
 
 echo "rsync server01 backup to latest folder COMPLETE!"
 echo "-"
 
 echo "STARTING rsync server02 backup to latest folder..."
-rsync -avhq --delete --delete-excluded --log-file=/mnt/external/backups/logs/server_secondary.log pi@192.168.86.102:/opt/server_secondary/ /mnt/external/backups/latest/server02
+rsync -avhq --delete --delete-excluded pi@192.168.86.102:/opt /mnt/external/backups/latest/server02
 
 echo "rsync from server02 COMPLETE"
 echo "-"
 
 echo "STARTING restic backup to external repo..."
-sudo restic --password-file /mnt/external/backups/server_restic/.secret -r /mnt/external/backups/server_restic backup --tag SCRIPT --files-from /home/pi/backup_script/restic_include.dat --exclude-file /home/pi/backup_script/restic_exclude.dat
+sudo restic --password-file /mnt/external/backups/server_restic/.secret -r /mnt/external/backups/server_restic backup --tag SCRIPT --files-from ./restic_include.dat --exclude-file ./restic_exclude.dat
 
 echo "restic server backup to repo COMPLETE!"
 echo "-"
